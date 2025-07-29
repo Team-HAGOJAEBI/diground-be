e572a05108fb10d3a135af9c5526dab4a4aa1e0b
chb6734 <chb6734@gmail.com>
2025-07-29 17:57:52 +0900

    chore: NestJS config Module Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //config 모듈에서 가져온 모든 설정을 전역적으로 사용 가능함.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
