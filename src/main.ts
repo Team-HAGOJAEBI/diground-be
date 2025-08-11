3d345a37fe980d65b00e73b1223f054a6148cbee
chb6734 <chb6734@gmail.com>
2025-08-12 01:22:30 +0900

    feat: Swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Diground API')
+    .setDescription('The Diground API description')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+      name: 'access-token',
+      description: 'Enter access token',
+    },
+      'access-token',
+      ).build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
